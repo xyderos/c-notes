a4b095914fc1896f507e0170e72b54a9ff4c28e6
konstantinos.xyderos <konstantinos.xyderos@knightec.se>
2023-02-28 13:42:14 +0100

    added basic notes on memory management

--- tests/driver.c
+++ tests/driver.c
@@ -6,16 +6,15 @@
 #include "driver.h"
 
 static Suite *
-calculations_suite(void)
+memory_suite(void)
 {
 	Suite *s = NULL;
 	TCase *tc_core = NULL;
 
-	s = suite_create("calculation");
+	s = suite_create("memory");
 	tc_core = tcase_create("core");
 
-	tcase_add_test(tc_core, TEST_ADDITION_SHOULD_BE_OKAY);
-	tcase_add_test(tc_core, TEST_MULTIPLICATION_SHOULD_BE_OKAY);
+	tcase_add_test(tc_core, TEST_ALIGNED_MALLOC_SHOULD_BE_OKAY);
 
 	suite_add_tcase(s, tc_core);
 
@@ -30,7 +29,7 @@ main(void)
 	Suite *s = NULL;
 	SRunner *runner = NULL;
 
-	s = calculations_suite();
+	s = memory_suite();
 	runner = srunner_create(s);
 
 	srunner_run_all(runner, CK_NORMAL);
--- tests/driver.h
+++ tests/driver.h
@@ -1,17 +1,14 @@
 #include <check.h>
+#include <stdlib.h>
 
-START_TEST(TEST_MULTIPLICATION_SHOULD_BE_OKAY)
+#include "../src/memory.h"
+
+START_TEST(TEST_ALIGNED_MALLOC_SHOULD_BE_OKAY)
 {
-	int result = multiplication(1, 2);
+	void *mem = aligned_memory(1024);
 
-	ck_assert_int_eq(2, result);
-}
-END_TEST
+	ck_assert_ptr_nonnull(mem);
 
-START_TEST(TEST_ADDITION_SHOULD_BE_OKAY)
-{
-	int result = addition(1, 1);
-
-	ck_assert_int_eq(2, result);
+	free(mem);
 }
 END_TEST
