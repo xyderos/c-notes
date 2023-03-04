e28090f73c70185bfc67daf4dc5975ce7723ea4d
xyderos <kostas@xyderos.com>
2023-03-04 18:31:23 +0100

    restructured undefined behavior

--- tests/driver.h
+++ tests/driver.h
@@ -1,13 +1,1 @@
 #include <check.h>
-
-START_TEST(TEST_MULTIPLICATION_SHOULD_BE_OKAY)
-{
-	ck_assert_int_eq(2, 2);
-}
-END_TEST
-
-START_TEST(TEST_ADDITION_SHOULD_BE_OKAY)
-{
-	ck_assert_int_eq(2, 2);
-}
-END_TEST
