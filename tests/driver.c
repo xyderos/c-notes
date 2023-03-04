50b44621a1d182c8949e1b4d14fedb75112f9966
xyderos <kostas@xyderos.com>
2023-03-04 18:24:28 +0100

    added arrays

--- tests/driver.c
+++ tests/driver.c
@@ -3,22 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "driver.h"
-
-static Suite *
-calculations_suite(void)
-{
-	Suite *s = NULL;
-	TCase *tc_core = NULL;
-
-	s = suite_create("calculation");
-	tc_core = tcase_create("core");
-
-	suite_add_tcase(s, tc_core);
-
-	return s;
-}
-
 int
 main(void)
 {
@@ -27,12 +11,11 @@ main(void)
 	Suite *s = NULL;
 	SRunner *runner = NULL;
 
-	s = calculations_suite();
-	runner = srunner_create(s);
+        runner = srunner_create(s);
 
-	srunner_run_all(runner, CK_NORMAL);
-	no_failed = srunner_ntests_failed(runner);
-	srunner_free(runner);
+        srunner_run_all(runner, CK_NORMAL);
+        no_failed = srunner_ntests_failed(runner);
+        srunner_free(runner);
 
-	return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+        return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
