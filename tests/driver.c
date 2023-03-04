ceadcf90a1626b00d17a1ab1a7514e02550c6d54
xyderos <kostas@xyderos.com>
2023-03-04 16:30:46 +0100

    addes strings

--- tests/driver.c
+++ tests/driver.c
@@ -1,31 +1,12 @@
+#include "driver.h"
+
 #include <check.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "driver.h"
-
-static Suite *
-strings_suite(void)
-{
-	Suite *s = NULL;
-	TCase *tc_core = NULL;
-
-	s = suite_create("strings");
-	tc_core = tcase_create("core");
-
-	tcase_add_test(tc_core,
-	    TEST_COMMAND_PARSER_WITH_STRTOK_R_SHOULD_BE_OKAY);
-	tcase_add_test(tc_core, TEST_COMMAND_PARSER_WITH_STRTOK_SHOULD_BE_OKAY);
-	tcase_add_test(tc_core, ITERATING_OVER_A_STRING_SHOULD_BE_OKAY);
-	tcase_add_test(tc_core, GET_FILE_NAME_WITH_STRCHR_SHOULD_BE_OKAY);
-	tcase_add_test(tc_core, TRY_PARSE_LONG_SHOULD_BE_OKAY);
-
-	suite_add_tcase(s, tc_core);
-
-	return s;
-}
+#include "test_strings/test_strings.h"
 
 int
 main(void)
