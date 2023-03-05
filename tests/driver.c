f2f0bde61c68665e4ae4d00123ac2404543eb667
xyderos <kostas@xyderos.com>
2023-03-05 09:45:02 +0100

    restructed assembly

--- tests/driver.c
+++ tests/driver.c
@@ -3,23 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "driver.h"
-
-static Suite *
-assembly_suite(void)
-{
-	Suite *s = NULL;
-	TCase *tc_core = NULL;
-
-	s = suite_create("assembly");
-	tc_core = tcase_create("core");
-
-	tcase_add_test(tc_core, TEST_SWAP_IN_ASSEMBLY_SHOULD_BE_OKAY);
-
-	suite_add_tcase(s, tc_core);
-
-	return s;
-}
+#include "test_assembly/test_assembly.h"
 
 int
 main(void)
@@ -29,12 +13,11 @@ main(void)
 	Suite *s = NULL;
 	SRunner *runner = NULL;
 
-	s = assembly_suite();
-	runner = srunner_create(s);
+        runner = srunner_create(assembly_suite());
 
-	srunner_run_all(runner, CK_NORMAL);
-	no_failed = srunner_ntests_failed(runner);
-	srunner_free(runner);
+        srunner_run_all(runner, CK_NORMAL);
+        no_failed = srunner_ntests_failed(runner);
+        srunner_free(runner);
 
-	return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+        return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
---
+++ tests/test_assembly/test_assembly.h
@@ -0,0 +1,16 @@
+#include <check.h>
+
+#include "../../src/assembly/assembly.h"
+
+Suite *assembly_suite(void);
+
+START_TEST(TEST_SWAP_IN_ASSEMBLY_SHOULD_BE_OKAY)
+{
+	int n1 = 1, n2 = 2;
+	int *p1 = &n1, *p2 = &n2;
+	swap(p1, p2);
+
+	ck_assert_int_eq(2, n1);
+	ck_assert_int_eq(1, n2);
+}
+END_TEST
